c96a476633bd69e408e1bd120acbcfb16fed5748
Oliver Dietrich <Oliver.Dietrich@helmholtz-hiri.de>
2022-09-30 17:12:38 +0200

    Finished dataset-combined.py. Update _template.yml.

--- bin/dataset-combined.py
+++ bin/dataset-combined.py
@@ -1,15 +1,44 @@
-# Setup the combined BAL samples as a Anndata object
+#!/usr/bin/env python3
 
-# Import packages --------------------------------------------------------------
+"""
+Download the combined BAL dataset
+
+Usage:
+    dataset-combined.py [options]
+    
+Options:
+    -h --help           Show this screen.
+    -o --overwrite      Overwrite existing file.
+"""
+
+import os
+import os.path
+import tempfile
 import scanpy as sc
+from docopt import docopt
 
-# Global variables -------------------------------------------------------------
-fromfile = "data/combined/filtered_feature_bc_matrix.h5"
-tofile = "data/combined/anndata.h5ad"
+# Global settings
+url = "https://nubes.helmholtz-berlin.de/s/A7XQ3yZYGMnFR3H"
+url = os.path.join(url, "download")
+out_file = "data/combined.h5ad"
 
-# Load data --------------------------------------------------------------------
-ad = sc.read_10x_h5(fromfile)
-ad.var_names_make_unique()
+# Check file presence / overwrite
+args = docopt(__doc__)
+if (os.path.exists(out_file) and not args["--overwrite"]):
+    print(f"'{out_file}' already exists. Exiting")
+    exit(2)
 
-# Save data
-ad.write_h5ad(tofile)
+print(f"Downloading matrix from '{url}'")
+temp_dir = tempfile.TemporaryDirectory()
+temp_file = os.path.join(temp_dir.name, "matrix.h5")
+call = "curl" + " " + url + " -o " + temp_file
+os.system(call)
+
+print("Create AnnData object")
+adata = sc.read_10x_h5(temp_file)
+adata.var_names_make_unique()
+
+temp_dir.cleanup()
+
+print(f"Saving AnnData to '{out_file}'")
+adata.write_h5ad(out_file)
