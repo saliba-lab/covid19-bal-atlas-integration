a580bfdc4142ba3104e8df02e07cf6987855ca5b
OliverDietrich <Oliver.Dietrich@helmholtz-hiri.de>
2022-12-05 19:27:22 +0100

    Create viral genome FASTA & GTF.

--- bin/preprocessing/download_viral-genomes.R
+++ bin/preprocessing/download_viral-genomes.R
@@ -1,14 +1,18 @@
 # Download viral genomes
 
 # Set paths
-file <- "docs/viral-assembly_genome-urls.csv"
-dir <- "data/genomes/viral/"
-dir.create(dir)
+file <- "docs/download/viral-assembly_genome-urls.csv"
+out.fa <- "docs/genomes/viral.fasta"
+out.gtf <- "docs/genomes/viral.gtf"
+dir <- "docs/genomes/"
+dir.create(dir, recursive = TRUE)
 
 # Load data
 list <- read.csv(file)
 
-# Download files
+# Download files ---------------------------------------------------------------
+message("Downloading FASTA & GTF files...")
+
 for (i in 1:nrow(list)) {
   name <- list[i, "name"]
   type <- list[i, "type"]
@@ -20,3 +24,17 @@ for (i in 1:nrow(list)) {
   
   download.file(url, key)
 }
+
+# Combined FASTA files ---------------------------------------------------------
+message("Combining FASTA files...")
+
+call <- paste0("cat ", dir, "*.fasta", " > ", out.fa)
+system(call)
+
+# Create GTF for all virus genomes ---------------------------------------------
+message("Creating GTF file...")
+
+# TODO: Create gtf from gff3 files
+# So far done manually...
+
+message("Done.")
